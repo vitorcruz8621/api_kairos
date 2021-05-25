4d70d039afc3aba9f77a78f43ceb2acfe10ab5a3
vitorcruz8621 <vitorcandrade98@gmail.com>
2021-05-25 15:47:00 -0300

    Atualização 2021.05.25

--- mainSGC.js
+++ mainSGC.js
@@ -1,66 +1,157 @@
-class horariosSGC {
+class HorariosSGC {
     marcacoes
+    saldoDiario
+    saldoDoMes
 
     constructor(){
         this.marcacoes = this.retornarMarcacoes()
+        this.saldoDiario = this.marcacoes.map(element => element.marcacaoHorario.saldoDoDia)
+        this.saldoDoMes = (() => {
+            var totalMinutosSaldoMes = (this.saldoDiario.map(elemento => parseInt(elemento.substring(0,2)) * 60 +  parseInt(elemento.substring(3,5))))
+                .reduce((total, atual) => total + atual)
+            var horasSaldoMes = Math.floor(totalMinutosSaldoMes / 60);
+            var minutosSaldoMes = totalMinutosSaldoMes - (horasSaldoMes * 60)
+
+            return (horasSaldoMes.toString()).concat(":").concat((minutosSaldoMes.toString()).padStart(2, "0") );
+        })()
     }
     
     retornarMarcacoes() {
-        var arrHoras = []
-        const arrHorasMisturadas = [...document.querySelector("#chamadoIt > tbody").rows].map(element => {
-            return {
-                'data' : element.querySelector("td:nth-child(1)").innerText,
-                'horas' : element.querySelector("td:nth-child(2)").innerText
-            }
-        });
+        console.log("Executando: retornarMarcacoes");
 
-        function horarioString(tempo,habilitarSinalPositivo){
+        function adicionarUmDia(keyData) {
+            console.log("Executando: adicionarUmDia");
+            console.log("Finalizando: adicionarUmDia");
+            return ((parseInt(keyData.substring(0,2)))+1).toString().padStart(2,"0").concat(keyData.substring(2,10))
+        }
+        
+        function validarBatidasERetoranrDiaFiltradoDiaFiltrado (arrHorasMisturadas, keyData) {
+            console.log("Executando: validarBatidasERetoranrDiaFiltradoDiaFiltrado");
+            const MOVIMENTO_ENTRADA = "ENTRADA", MOVIMENTO_SAIDA = "SAIDA";
+
+            var objAtual = arrHorasMisturadas.filter( (element, index) => {
+                if (element.data === keyData) {
+                    if (index % 2 === 0 && element.movimento === MOVIMENTO_SAIDA)
+                        throw new Error("A marcação '"+element.data+" "+element.marcacaoHorario+" "+element.movimento+"' deveria ter o movimento '"+MOVIMENTO_ENTRADA+"'.");
+                    else if (index % 2 === 1 && element.movimento !== "SAIDA")
+                        throw new Error("A marcação '"+element.data+" "+element.marcacaoHorario+" "+element.movimento+"' deveria ter o movimento '"+MOVIMENTO_SAIDA+"'.");
+                    else 
+                        return element
+                }
+            })
+
+            if (objAtual.length % 2 !== 0 ) {
+                throw new Error("A data "+ keyData +" possue uma quantidade ímpar de marcações.")
+            }
+
+            if (objAtual === [] || objAtual.length === 0) {
+                return null;
+            }
+
+            console.log("Finalizando: validarBatidasERetoranrDiaFiltradoDiaFiltrado");
+            return objAtual;
+        }
+        
+        function retornarArrDiasTrabalhadosValido(pKeyDataMin, pKeyDataMax, pArrHorasMisturadas) {
+            console.log("Executando: retornarArrDiasTrabalhadosValido");
+            var arrJsonMarcacoes = [];
+
+            for (var dataAtual = pKeyDataMin; dataAtual <= pKeyDataMax; dataAtual = adicionarUmDia(dataAtual) ) {
+                //var diaAtual = (new Date(dataAtual.substring(6,10), (parseInt(dataAtual.substring(3,5)) - 1), dataAtual.substring(0,2) )).getDay()
+                //if ( !( /0|6/.test(diaAtual) )) {//
+                    var objAtual = validarBatidasERetoranrDiaFiltradoDiaFiltrado(pArrHorasMisturadas, dataAtual)
+
+                    if ( objAtual === null) {
+                        continue
+                    } else {
+                        objAtual = retornarBatidasComoDiaTrabalhadoJson(objAtual);
+                        arrJsonMarcacoes.push(objAtual);
+                    }
+               // }
+            }
+
+            console.log("Finalizando: retornarArrDiasTrabalhadosValido");
+
+            return arrJsonMarcacoes;
+        }
+
+        function montarMarcacaoeSaldoDoDia(pDataAtualMarcacoes) {
+            var saldoDoDia = 0;
+            var arrayMarcacoes = [];
+            console.log("Executando: montarMarcacaoeSaldoDoDia");
+
+            for (var cont =0; cont < pDataAtualMarcacoes.length; cont = cont + 2) {
+                var horaEntrada = pDataAtualMarcacoes[cont]
+                var horaSaida = pDataAtualMarcacoes[cont+1]
+
+                saldoDoDia += (
+                    ((parseInt(horaSaida.marcacaoHorario.substring(0,2)) * 60) + 
+                    (parseInt(horaSaida.marcacaoHorario.substring(3,5)))) - 
+                    ((parseInt(horaEntrada.marcacaoHorario.substring(0,2)) * 60) + 
+                    (parseInt(horaEntrada.marcacaoHorario.substring(3,5))))
+                )
+
+                arrayMarcacoes.push(horaEntrada);
+                arrayMarcacoes.push(horaSaida);
+            }
+
+            console.log("Finalizando: montarMarcacaoeSaldoDoDia");
+
+            return {
+                'listaMarcacoes' : arrayMarcacoes,
+                'saldoDoDia' : retornarCalculoHorarioComoString(saldoDoDia, false),
+                'saldoDoDiaDiferenca' : retornarCalculoHorarioComoString(saldoDoDia - 480, true)
+            }
+        }
+
+        function retornarBatidasComoDiaTrabalhadoJson(pDataAtualMarcacoes) {
+            console.log("Executando: retornarBatidasComoDiaTrabalhadoJson");
+
+            var batidas = {
+                'data' : pDataAtualMarcacoes[0].data,
+                'marcacaoHorario' : montarMarcacaoeSaldoDoDia(pDataAtualMarcacoes)
+            }
+
+            console.log("Finalizando: retornarBatidasComoDiaTrabalhadoJson");
+
+            return batidas
+        }
+
+        function retornarCalculoHorarioComoString(tempo,habilitarSinalPositivo){
+            console.log("Executando: retornarCalculoHorarioComoString");
             var horas = Math.floor(Math.abs(tempo)/60 )
             var minutos = Math.abs(Math.abs(tempo) - (horas * 60) )
             var sinal = "";
             
-            if (tempo > 0 && habilitarSinalPositivo) sinal = "+";
+            if (tempo >= 0 && habilitarSinalPositivo) sinal = "+";
             else if ( tempo < 0) sinal = "-";
+
+            console.log("Finalizando: retornarCalculoHorarioComoString");
+
             return ( sinal + ( (horas.toString()).padStart(2, "0") ).concat(":").concat( (minutos.toString()).padStart(2, "0")) );
         }
-    
-        for (var cont = 0; cont < ((arrHorasMisturadas.length)); cont=cont+4) {
-            var horas = 0;
-            var minutos = (
-                (
-                    (
-                        (parseInt(arrHorasMisturadas[cont+3].horas.substring(0,2)) * 60) 
-                        + (parseInt(arrHorasMisturadas[cont+3].horas.substring(3,5)))
-                    ) 
-                    - 
-                    (
-                        (parseInt(arrHorasMisturadas[cont+2].horas.substring(0,2)) * 60) 
-                        + (parseInt(arrHorasMisturadas[cont+2].horas.substring(3,5)))
-                    )
-                )
-                + 
-                (
-                    (
-                        (parseInt(arrHorasMisturadas[cont+1].horas.substring(0,2)) * 60) 
-                        + (parseInt(arrHorasMisturadas[cont+1].horas.substring(3,5))) ) 
-                    - 
-                    ( 
-                        (parseInt(arrHorasMisturadas[cont+0].horas.substring(0,2)) * 60) 
-                        + (parseInt(arrHorasMisturadas[cont+0].horas.substring(3,5)))
-                    )
-                )
-            )
-    
-            arrHoras.push({
-                'data' : arrHorasMisturadas[cont].data,
-                'horario' : horarioString(minutos, false),
-                'diferencaSaldoDia' : horarioString(minutos - 480, true)
-                //'horario' : ( (horas.toString()).padStart(2, "0") ).concat(":").concat( (minutos.toString()).padStart(2, "0") )
-            })
-        }
-    
+
+        var arrHoras = []
+        const arrHorasMisturadas = [...document.getElementById("chamadoIt").tBodies.item(0).rows].map(element => {
+            
+            return {
+                'data' : element.querySelector("td:nth-child(1)").innerText,
+                'marcacaoHorario' : element.querySelector("td:nth-child(2)").innerText,
+                'movimento' : element.querySelector("td:nth-child(3)").innerText,
+                'statusAutorizacao' : element.querySelector("td:nth-child(4)").innerText,
+                'localidade' : element.querySelector("td:nth-child(5)").innerText,
+                'justificativa' : element.querySelector("td:nth-child(6)").innerText
+            }
+        });
+
+        arrHoras = retornarArrDiasTrabalhadosValido(
+            arrHorasMisturadas[0].data, 
+            arrHorasMisturadas[arrHorasMisturadas.length-1].data, 
+            arrHorasMisturadas)
+
+        console.log("Finalizando: retornarMarcacoes");
         return arrHoras
     }
 }
 
-var main = new horariosSGC()
+var main = new HorariosSGC()
